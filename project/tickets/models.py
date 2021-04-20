d006adaa9ecce034583ede1eebc3d65f57a25cad
vgkienzler <vkzr79@gmail.com>
2021-04-20 23:20:24 +0300

    cleaning before starting

--- project/tickets/models.py
+++ project/tickets/models.py
@@ -1,3 +1,19 @@
+# tickets/models.py
+
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Ticket(models.Model):
+    summary = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now=True)
+    author_name = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, default='new')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.summary
+
+    class Meta:
+        ordering = ['-created_on']
--- project/tickets/views.py
+++ project/tickets/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+from . import models
+
 
 # Create your views here.
+class TicketCreateView(generic.CreateView):
+    model = models.Ticket
+    fields = ('summary', 'description', 'author_name')
+    success_url = reverse_lazy('tickets:list')
+
+    # def get_success_url(self):
+    #     return reverse('lawyer_detail', kwargs={'lawyer_slug': self.object.lawyer_slug})
+
+    # def form_valid(self, form):
+    #     self.object = form.save(commit=False)
+    #     # self.object.user = self.request.user
+    #     self.object.save()
+    #     return super().form_valid(form)
+
+
+class TicketsListView(generic.ListView):
+    model = models.Ticket
+
+
+class TicketDetailView(generic.DetailView):
+    model = models.Ticket
