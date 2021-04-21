db2367f627afe3d6c8120ec54e0c08b3313ebed4
vgkienzler <vkzr79@gmail.com>
2021-04-21 20:17:27 +0300

    chore: cleaning before starting 2
    Commiting forgotten changes from previous commit
    Moving public to project
    Update .gitignore
    Update settings.py

--- project/tickets/urls.py
+++ project/tickets/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'tickets'
 
 urlpatterns = [
+    path('', views.TicketsListView.as_view(), name='default'),
     path('list/', views.TicketsListView.as_view(), name='list'),
     path('create/', views.TicketCreateView.as_view(), name='create'),
     path('<int:pk>/', views.TicketDetailView.as_view(), name='detail'),
